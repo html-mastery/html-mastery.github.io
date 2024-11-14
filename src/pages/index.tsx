65444c9600aae8ec28fb04a26fd73f58ca127b2a
Ajay-Dhangar <ajaydhangar49@gmail.com>
2024-11-14 23:40:58 +0530

    added more docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="HTML Mastery is a free, open-source, online resource for learning HTML.">
       <HomepageHeader />
     </Layout>
   );
